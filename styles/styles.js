a5c932270553ac370172aa7591de0491038e8dc3
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-12 12:21:56 -0700

    needs more editing. getting two answers to prompts

--- styles/styles.js
+++ styles/styles.js
@@ -3,14 +3,19 @@ const PI = 3.1415;
 const totalArea = PI * R * R;
 const minSpace = 0.8;
 
-let week = 0;
+let week = 2;
 let plants = 20;
-
-
-let week = 0
-let plants = 20
 // let plantspace = 0.8
 let plantsByWeek = plants * (2 ** week)
-// let plantByWeekArea
+let plantsAreaByWeek = plantsByWeek * 0.8
 
 console.log(plantsByWeek)
+console.log(plantsAreaByWeek)
+
+if (plantsAreaByWeek >= totalArea * 0.8) {
+    console.log(`Prune the plants due to ${plantsAreaByWeek} is more than 80% of ${totalArea}`);
+} else if (plantsAreaByWeek > totalArea * 0.5 && plantsAreaByWeek < totalArea * 0.8) {
+    console.log(`Monitor the plants due to ${plantsAreaByWeek} is less than 80% and more than 50% ${totalArea}`);
+} else (plantsAreaByWeek <= totalArea * 0.5);
+    console.log(`Plant more plants due to ${plantsAreaByWeek} is less than 80% and more than 50% ${totalArea}`);
+
