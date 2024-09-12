c0708f6c60787c7a0af664d5e84449ff86bdd1fb
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-12 10:42:20 -0700

    plant total area

---
+++ styles/styles.js
@@ -0,0 +1,16 @@
+const R = 5;
+const PI = 3.1415;
+const totalArea = PI * R * R;
+const minSpace = 0.8;
+
+let week = 0;
+let plants = 20;
+
+
+let week = 0
+let plants = 20
+// let plantspace = 0.8
+let plantsByWeek = plants * (2 ** week)
+// let plantByWeekArea
+
+console.log(plantsByWeek)
